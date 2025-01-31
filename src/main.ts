f48391f66562f5a14b45427e588126c2c67dfd34
Grecia Delgado <greciadm2596@gmail.com>
2025-01-31 12:04:49 -0500

    Remove inncesary doc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config';
 
@@ -14,6 +14,13 @@ async function bootstrap() {
         port: envs.port,
       },
     },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen();
